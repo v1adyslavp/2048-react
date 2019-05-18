37debe58e8cd2fbd1c89b7c8f5b6de3743d7c36d
Vladik <optimus014@gmail.com>
2019-05-18 21:56:07 +0300

    Updates

---
+++ precache-manifest.f8991bf24639725e159cb51becf8ef2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f48a01d228e0011f79f9c90f7ffe4eb",
+    "url": "/2048-react/index.html"
+  },
+  {
+    "revision": "fbeb6666594c5a86f96e",
+    "url": "/2048-react/static/css/main.b06f5cee.chunk.css"
+  },
+  {
+    "revision": "4b35cec110dae551edb7",
+    "url": "/2048-react/static/js/2.f30633a9.chunk.js"
+  },
+  {
+    "revision": "fbeb6666594c5a86f96e",
+    "url": "/2048-react/static/js/main.fedcc471.chunk.js"
+  },
+  {
+    "revision": "62588263e4f4f93042c5",
+    "url": "/2048-react/static/js/runtime~main.fe393d6e.js"
+  }
+]);
