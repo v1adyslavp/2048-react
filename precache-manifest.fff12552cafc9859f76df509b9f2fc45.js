bae6ed28121a8ede2bdd84eadf3a20d56130dcbf
Vladik <optimus014@gmail.com>
2019-05-19 17:35:46 +0300

    Updates

--- precache-manifest.fff12552cafc9859f76df509b9f2fc45.js
+++ precache-manifest.fff12552cafc9859f76df509b9f2fc45.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "96dd2e43eb5880ceb2754b2c9c356dd8",
+    "revision": "ee7dbcb5df2c3b9f9822d18d25d7f384",
     "url": "/2048-react/index.html"
   },
   {
-    "revision": "68880846ea54329ce8a2",
-    "url": "/2048-react/static/css/main.b06f5cee.chunk.css"
+    "revision": "a077a649b358d5687d25",
+    "url": "/2048-react/static/css/main.e4d5dd6f.chunk.css"
   },
   {
     "revision": "c656996354036ba0e190",
     "url": "/2048-react/static/js/2.f79a5448.chunk.js"
   },
   {
-    "revision": "68880846ea54329ce8a2",
-    "url": "/2048-react/static/js/main.91f684a5.chunk.js"
+    "revision": "a077a649b358d5687d25",
+    "url": "/2048-react/static/js/main.24933597.chunk.js"
   },
   {
     "revision": "62588263e4f4f93042c5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/2048-react/precache-manifest.43a4e54427064c42c7a16b99e3d077eb.js"
+  "/2048-react/precache-manifest.fff12552cafc9859f76df509b9f2fc45.js"
 );
 
 self.addEventListener('message', (event) => {
